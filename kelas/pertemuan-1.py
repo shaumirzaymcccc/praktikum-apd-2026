0d6d160b2f8c92f59eed14c1200d5d47765074fe
shaumirzaymcccc <shaumirzaym@gmail.com>
2026-09-19 17:19:01 +0800

    Hapus main.py kosong, tambah file kelas dan posttest

---
+++ kelas/pertemuan-1.py
@@ -0,0 +1,53 @@
+# a = 5
+# b = 10
+# c = a + b
+# print(c)
+
+# a = 5.5
+# b = 10.3
+# c = a + b
+# print(c)
+
+# nama = "Larry"
+# hobi = "ngoding"
+# print(nama)
+# print(hobi)
+
+# print('''Halo saya Rendy
+# Saya suka belajar coding
+# Hari ini saya belajar bahasa pemrograman Python''')
+
+# print("aku""rajin""praktikum")
+# print("aku"+"rajin")
+
+# teks = "Rahmat Flowchart"
+# print(teks[3])
+# print(teks[3:6])
+# print(teks[7:])
+# print(teks[:6])
+
+# mata_kuliah = ["APD","Logika Matematika","Kalkulus"]
+# print(mata_kuliah[0])
+# print(mata_kuliah[1])
+# print(mata_kuliah[2])
+
+# a = "20"
+# print(type(a))
+
+# nama = input("Masukan nama anda")
+# print(nama)
+
+# a = 20
+# b = 30
+# c = a * b
+# print(c)
+
+# a = 2
+# b = "20"
+# c = a * b
+# print(c)
+
+# if 20 < 10 or 20 > 10:
+#     print("bener banget")
+# else:
+#     print("salah woy")
